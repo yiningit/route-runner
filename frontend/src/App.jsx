1c767369899baf518cb4417d68252c105f0414a8
yiningit <145828510+yiningit@users.noreply.github.com>
2026-04-18 15:52:15 +1000

    Init frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+
+export default function App() {
+  return <h1>Route Runner MVP 🚴</h1>;
+}
